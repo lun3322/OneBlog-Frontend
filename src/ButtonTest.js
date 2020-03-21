3a069b111ea65a62ffdb8eefa6ee445d2d9655ed
hong <lun3322@126.com>
2020-03-22 00:05:05 +0800

    eject antd less

---
+++ src/ButtonTest.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Button} from "antd";
+
+class ButtonTest extends Component {
+    render() {
+        return (
+            <div>
+                <Button type="primary">测试</Button>
+            </div>
+        );
+    }
+}
+
+export default ButtonTest;
