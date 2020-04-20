393c796106c50f3078d61eaa0ba2dea33a3756f3
hong <lun3322@126.com>
2020-04-20 23:51:17 +0800

    更新最小高度

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
               <Breadcrumb.Item>User</Breadcrumb.Item>
               <Breadcrumb.Item>Bill</Breadcrumb.Item>
             </Breadcrumb>
-            <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
+            <div className="site-layout-background" style={{padding: 24, minHeight: '100%'}}>
               Bill is a cat.
             </div>
           </Content>
